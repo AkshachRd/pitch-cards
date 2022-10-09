cb18b9931627dc57f3dd4faadefc9390f5814a78
Daniil <dr.chashka@gmail.com>
2022-10-09 09:32:02 +0300

    Рефактеринг тайпгардов

--- src/shared/lib/typeGuards/index.ts
+++ src/shared/lib/typeGuards/index.ts
@@ -1,7 +1,8 @@
 import {ArtObject, CanvasObject, ImageObject, TextObject} from "../../types";
 
 export const isArtObject = (obj: CanvasObject | ArtObject): obj is ArtObject => {
-    return (obj as ArtObject).shape !== undefined;
+    return (obj as ArtObject).shape !== undefined &&
+        (obj as ArtObject).color !== undefined;
 }
 
 export const isImageObject = (obj: CanvasObject | ImageObject): obj is ImageObject => {
@@ -9,5 +10,9 @@ export const isImageObject = (obj: CanvasObject | ImageObject): obj is ImageObje
 }
 
 export const isTextObject = (obj: CanvasObject | TextObject): obj is TextObject => {
-    return (obj as TextObject).content !== undefined;
+    return (obj as TextObject).content !== undefined &&
+        (obj as TextObject).style !== undefined &&
+        (obj as TextObject).fontFamily !== undefined &&
+        (obj as TextObject).fontSize !== undefined &&
+        (obj as TextObject).color !== undefined;
 }
