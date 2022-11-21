f66f317a37065a65fa3a76894e6c1452f5f3bc0a
Daniil <dr.chashka@gmail.com>
2022-11-21 22:34:37 +0300

    Рефактеринг функции получения размера текста.

--- src/shared/lib/canvas/getTextSize.ts
+++ src/shared/lib/canvas/getTextSize.ts
@@ -1,11 +1,14 @@
 import {Size} from "shared/types";
 
 export const getTextSize = (content: string, font: string): Size => {
-    const ctx = document.createElement("canvas").getContext("2d") as CanvasRenderingContext2D;
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
     ctx.font = font;
 
     const metrics = ctx.measureText(content);
     const height = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    canvas.remove();
 
     return {width: metrics.width, height};
 };
